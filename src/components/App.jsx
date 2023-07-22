63f67a975759c861df6bde1da5226d754e0e1cc1
DmytroKrylov <120196125+DmytroKrylov@users.noreply.github.com>
2023-07-22 20:32:03 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { searchImages } from 'services/searchImage';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -13,41 +13,42 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (value) {
-      getImages();
+    if (!value) return;
+
+    async function searchImageHandler() {
+      try {
+        setIsLoading(true);
+        const resp = await searchImages(value, page);
+
+        const { totalHits, hits } = resp.data;
+
+        if (!Array.isArray(hits)) {
+          console.error('Invalid response format: hits is not an array');
+          setIsLoading(false);
+          return;
+        }
+
+        const data = hits.map(({ id, webformatURL, largeImageURL, tags }) => {
+          return {
+            id,
+            webformatURL,
+            largeImageURL,
+            tags,
+          };
+        });
+
+        const totalPages = Math.ceil(totalHits / 12);
+        setImages(images => [...images, ...data]);
+        setShowButton(page < totalPages || page === 0);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
+
+    searchImageHandler();
   }, [page, value]);
-
-  const searchImageHandler = async () => {
-    try {
-      setIsLoading(true);
-      const response = await searchImages(value, page);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const getImages = async () => {
-    const resp = await searchImageHandler();
-    const data = resp.hits.map(({ id, webformatURL, largeImageURL, tags }) => ({
-      id,
-      webformatURL,
-      largeImageURL,
-      tags,
-    }));
-    const totalHits = resp.totalHits;
-    const totalPages = Math.ceil(totalHits / 12);
-
-    setImages(prevImages => [...prevImages, ...data]);
-    setShowButton(page < totalPages);
-  };
-
-  const onClickLoadMore = () => {
-    setPage(prevPage => prevPage + 1);
-  };
 
   const handlerSearch = value => {
     setValue(value);
@@ -56,10 +57,10 @@ const App = () => {
   };
 
   return (
-    <div className="">
+    <div>
       <Searchbar onSearch={handlerSearch} />
       <ImageGallery images={images} />
-      {showButton && <Button onClick={onClickLoadMore} />}
+      {showButton && <Button onClick={() => setPage(page => page + 1)} />}
       {isLoading && <Loader />}
     </div>
   );
